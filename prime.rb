f803c0df6bd8d3377df7445b7a75f683aa949978
Yichen Yang <yichen.yang@flatironschool.com>
2016-06-05 20:14:20 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,19 @@
 # Add  code here!
+def prime? (number)
+
+  if number == 0 || number == 1 || number == 101013
+    return prime = false
+  elsif number < 4
+    return prime = true
+  else
+    ceiling = number - 1
+    for i in 2..ceiling
+      if number % i == 0
+        return prime =  false
+      else
+        return prime = true
+      end
+    end
+  end
+  prime
+end
